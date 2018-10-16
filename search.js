22a452535c09190de1c9edc7fd3cbb2e097bb546
mkw98 <40563214+mkw98@users.noreply.github.com>
2018-10-16 14:08:13 +0200

    Update search.js

--- search.js
+++ search.js
@@ -1,4 +1,19 @@
 Search = React.createClass({
+    handleChange: function(event) {
+    var searchingText = event.target.value;
+    this.setState({searchingText: searchingText});
+
+    if (searchingText.length > 2) {
+      this.props.onSearch(searchingText);
+    }
+  },
+
+  handleKeyUp: function(event) {
+    if (event.keyCode === 13) {
+      this.props.onSearch(this.state.searchingText);
+    }
+  },
+    
     render: function() {
         var styles = {
             fontSize: '1.5em',
@@ -15,3 +30,10 @@ Search = React.createClass({
             />
   }
 });
+
+    
+    
+
+
+
+    
