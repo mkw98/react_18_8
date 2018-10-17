8ac364856e40f858963b8843a1d029d37ad79900
mkw98 <40563214+mkw98@users.noreply.github.com>
2018-10-17 12:06:50 +0200

    fix typo

--- components/App.js
+++ components/App.js
@@ -53,7 +53,7 @@ getGif: function(searchingText, callback) { // 1.
             };
         return (
           <div style={styles}>
-                <h1>Wyszukiwarka GIFow!</h1>
+                <h1>Wyszukiwarka GIFów!</h1>
                 <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
                 <Search onSearch={this.handleSearch}/>
             <Gif
